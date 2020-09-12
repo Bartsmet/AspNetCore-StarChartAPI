6f0299705b8f75c4af4b9fff4b4a9f3062557869
Bartsmet <44071078+Bartsmet@users.noreply.github.com>
2020-09-13 01:54:36 +0200

    art 5 get

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -23,6 +24,45 @@ namespace StarChart.Controllers
             _context = context;
         }
 
+        [HttpGet("{Id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+
+            CelestialObject obj = _context.CelestialObjects.Where(o => o.Id == id).FirstOrDefault();
+            
+
+            if (obj.Id != id)
+            {
+                return NotFound();
+            }
+
+            //List<CelestialObject> sat=_context.CelestialObjects.Where(o => o.Id == obj.OrbitedObjectId).ToList<>;
+
+            return Ok();
+        }
+
+        [HttpGet ("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            CelestialObject obj = _context.CelestialObjects.Where(o => o.Name == name).FirstOrDefault();
+
+            if (obj.Name != name)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            _context.CelestialObjects.Find();
+
+            return Ok();
+        }
 
 
     }
